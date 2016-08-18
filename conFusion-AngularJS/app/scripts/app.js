d35d462f807512ec1484e07d338c7bcfbacf7768
fzd9752 <fzd9752@msn.com>
2016-08-18 18:48:22 +0800

    Week 2 homework

--- conFusion-AngularJS/app/scripts/app.js
+++ conFusion-AngularJS/app/scripts/app.js
@@ -1,12 +1,14 @@
 'use strict';
+
 angular.module('confusionApp', [])
 
-        .controller('menuController',['$scope', function($scope) {
+        .controller('MenuController', ['$scope', function($scope) {
 
             $scope.tab = 1;
             $scope.filtText = '';
+            $scope.showDetails = false;
 
-            var dishes=[
+            $scope.dishes=[
                          {
                           name:'Uthapizza',
                           image: 'images/uthapizza.png',
@@ -17,37 +19,37 @@ angular.module('confusionApp', [])
                            comment: ''
                         },
                         {
-                           name:'Zucchipakoda',
-                           image: 'images/zucchipakoda.png',
-                           category: 'appetizer',
-                           label:'',
-                           price:'1.99',
+                          name:'Zucchipakoda',
+                          image: 'images/zucchipakoda.png',
+                          category: 'appetizer',
+                          label:'',
+                          price:'1.99',
                           description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
                            comment: ''
                         },
                         {
-                           name:'Vadonut',
-                           image: 'images/vadonut.png',
-                           category: 'appetizer',
-                           label:'New',
-                           price:'1.99',
+                          name:'Vadonut',
+                          image: 'images/vadonut.png',
+                          category: 'appetizer',
+                          label:'New',
+                          price:'1.99',
                           description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
                            comment: ''
                         },
                         {
-                           name:'ElaiCheese Cake',
-                           image: 'images/elaicheesecake.png',
+                          name:'ElaiCheese Cake',
+                          image: 'images/elaicheesecake.png',
                           category: 'dessert',
-                           label:'',
+                          label:'',
                           price:'2.99',
                           description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
                            comment: ''
                         }
                         ];
-             $scope.dishes = dishes;
 
-             $scope.select = function(setTab) {
+            $scope.select = function(setTab) {
                 $scope.tab = setTab;
+
                 if (setTab === 2) {
                     $scope.filtText = "appetizer";
                 }
@@ -61,11 +63,117 @@ angular.module('confusionApp', [])
                     $scope.filtText = "";
                 }
             };
+
             $scope.isSelected = function (checkTab) {
                 return ($scope.tab === checkTab);
             };
-            $scope.showDetails = false;
-            $scope.toggleDetails = function () {
-              $scope.showDetails = !$scope.showDetails;
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
             };
-        }]);
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+
+            $scope.sendFeedback = function() {
+
+                console.log($scope.feedback);
+
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', function($scope) {
+
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains',
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+
+                           ]
+                    };
+
+            $scope.dish = dish;
+
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+
+            $scope.newcomment = {rating:"5", comment:"", author:"", date:""};
+            //Step 1: Create a JavaScript object to hold the comment from the form
+
+            $scope.submitComment = function () {
+
+                console.log($scope.newcomment);
+
+                //Step 2: This is how you record the date
+                $scope.newcomment.date = new Date().toISOString();
+
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push($scope.newcomment);
+
+                //Step 4: reset your form to pristine
+                $scope.commentForm.$setPristine();
+
+                //Step 5: reset your JavaScript object that holds your comment
+                $scope.newcomment = {rating:"5", comment:"", author:"", date:""};
+                console.log($scope.newcomment);
+            };
+        }])
+
+;
