3355727be9cd5a31570f07e5ac96ecf664b0d202
fzd9752 <fzd9752@msn.com>
2016-09-04 15:34:23 +0800

    Yo and Yeoman

---
+++ AngularProject/app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularProjectApp
+ * @description
+ * # angularProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularProjectApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
